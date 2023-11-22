59d33ee0698241d88e262c7ec9c819dc33c0a522
shimjaedoo <woen1637@naver.com>
2023-11-23 03:37:19 +0900

    feat: 각 module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SurveyModule } from './survey/survey.module';
+import { QuestionModule } from './question/question.module';
+import { SelectModule } from './select/select.module';
+import { AnswerModule } from './answer/answer.module';
+import { GuestModule } from './guest/guest.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './_common/typeorm.config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SurveyModule,
+    QuestionModule,
+    SelectModule,
+    AnswerModule,
+    GuestModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+    GraphQLModule.forRoot({ autoSchemaFile: './src/schema.gql' }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
