be2dec0788b7288b5a9a12e962eaff10b4447641
shimjaedoo <woen1637@naver.com>
2023-11-23 03:20:58 +0900

    feat: Answer Entity

---
+++ src/answer/answer.entity.ts
@@ -0,0 +1,38 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Survey } from '../survey/survey.entity';
+import { Guest } from '../guest/guest.entity';
+import { Question } from '../question/question.entity';
+import { Select } from '../select/select.entity';
+
+@ObjectType()
+@Entity()
+export class Answer {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Field(() => Survey)
+  @ManyToOne(() => Survey, (survey) => survey.guests)
+  survey: Survey;
+
+  @Field(() => Guest)
+  @ManyToOne(() => Guest, (guest) => guest.answers)
+  guest: Guest;
+
+  @Field(() => Question)
+  @ManyToOne(() => Question, (question) => question.answers)
+  question: Question;
+
+  @Field(() => Select)
+  @ManyToOne(() => Select, (select) => select.answers)
+  select: Select;
+}
