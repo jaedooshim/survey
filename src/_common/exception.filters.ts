ec12e86d6609b9e840cdc5b046e444790a8892db
shimjaedoo <woen1637@naver.com>
2023-11-24 18:03:57 +0900

    feat: exception Filter

---
+++ src/_common/exception.filters.ts
@@ -0,0 +1,22 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    try {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const status = exception.getStatus(); // 예외코드
+      const message = exception.message;
+      response.status(status).json({ message });
+      console.log(exception);
+    } catch (err) {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+
+      const message = exception.message;
+      response.status(500).json({ message });
+      console.log(exception);
+    }
+  }
+}
