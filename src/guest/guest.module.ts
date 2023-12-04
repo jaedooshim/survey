5d0df618401a4b0ded7baf8582ebf3523a6b301b
shimjaedoo <woen1637@naver.com>
2023-12-04 14:43:47 +0900

    fix: Answer Entity import 추가

--- src/guest/guest.module.ts
+++ src/guest/guest.module.ts
@@ -4,9 +4,10 @@ import { GuestService } from './guest.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Survey } from '../survey/survey.entity';
 import { Guest } from './guest.entity';
+import { Answer } from '../answer/answer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Survey, Guest])],
+  imports: [TypeOrmModule.forFeature([Survey, Guest, Answer])],
   providers: [GuestResolver, GuestService],
   exports: [GuestService],
 })
